81b451b8b33a004aa16c15681a2340c2e462a43c
houmanave <uma.ps.anave.0901@gmail.com>
2019-04-01 22:22:30 +0900

    Add files via upload

---
+++ CeleryFunctions.cs
@@ -0,0 +1,91 @@
+﻿using Autodesk.DesignScript.Runtime;
+//using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Celery
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public class CeleryFunctions
+    {
+        public static double CeleryTangent(double angle)
+        {
+            //return Trig.Tan(angle);
+            return angle * angle;
+        }
+
+        /// <summary>
+        /// To be used for serialization
+        /// </summary>
+        /// <param name="p">A point</param>
+        /// <returns>A string of point coordinates.</returns>
+        public static string ConvertFromPointToString(Point p)
+        {
+            return p.X.ToString() + "," + p.Y.ToString();
+        }
+
+        /// <summary>
+        /// TO be used for deserialization
+        /// </summary>
+        /// <param name="value">A string of point coordinates.</param>
+        /// <returns>The converted point.</returns>
+        public static Point ConvertFromStringToPoint(string value)
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                string[] strspl = value.Split(',');
+                if (strspl.Length == 2)
+                {
+                    double x = Convert.ToDouble(strspl[0]);
+                    double y = Convert.ToDouble(strspl[1]);
+                    return new Point(x, y);
+                }
+            }
+
+            return new Point(0, 0);
+        }
+
+        public static Size MeasureString(string candidate, TextBlock tb = null)
+        {
+            Typeface tf = new Typeface("Arial");
+            if (tb != null)
+            {
+                tf = new Typeface(tb.FontFamily, tb.FontStyle, tb.FontWeight, tb.FontStretch);
+            }
+            var formattedText = new FormattedText(
+                candidate,
+                CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                tf,
+                8,
+                Brushes.Black);
+
+            return new Size(formattedText.Width, formattedText.Height);
+        }
+
+        public static FormattedText FormattedString(string candidate, TextBlock tb = null)
+        {
+            Typeface tf = new Typeface("Arial");
+            if (tb != null)
+            {
+                tf = new Typeface(tb.FontFamily, tb.FontStyle, tb.FontWeight, tb.FontStretch);
+            }
+            var formattedText = new FormattedText(
+                candidate,
+                CultureInfo.CurrentUICulture,
+                FlowDirection.LeftToRight,
+                tf,
+                8,
+                Brushes.Black);
+
+            return formattedText;
+        }
+    }
+}
---
+++ CeleryNodes.cs
@@ -0,0 +1,108 @@
+﻿using Dynamo.Engine;
+using Dynamo.Graph.Nodes;
+//using MathNet.Numerics;
+using ProtoCore.AST.AssociativeAST;
+using System;
+using System.Collections.Generic;
+
+namespace Celery
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CeleryNodes
+    {
+    }
+
+    /*[NodeName("Mathematics.Tangent")]
+    [NodeDescription("Evaluates the tangent of the number argument.")]
+    [NodeCategory("Celery.DeepMath")]
+    [InPortNames("arg")]
+    [InPortDescriptions("argument value")]
+    [InPortTypes("double")]
+    [OutPortNames("result")]
+    [OutPortDescriptions("Result from the tangent")]
+    [OutPortTypes("double")]
+    [IsDesignScriptCompatible]
+    public class Tangent : NodeModel
+    {
+        public Tangent()
+        {
+            RegisterAllPorts();
+
+            //ArgumentLacing = LacingStrategy.Disabled;
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            if (!HasConnectedInput(0))
+            {
+                return new[]
+                {
+                    AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode())
+                };
+            }
+
+            //Func<double, double> tangentmethod = Trig.Tan;
+
+            var functionnode = AstFactory.BuildFunctionCall(
+                new Func<double, double>(Trig.Tan),
+                new List<AssociativeNode>
+                {
+                    inputAstNodes[0]
+                });
+
+            return new[]
+                {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), functionnode)
+                };
+        }
+    }
+
+    [NodeName("Mathematics.FirstDerivative")]
+    [NodeDescription("Evaluates the first derivation of the function or equation.")]
+    [NodeCategory("Celery.DeepMath")]
+    [InPortNames("f(x)", "arg")]
+    [InPortDescriptions("Function or equation", "argument value")]
+    [InPortTypes("function", "double")]
+    [OutPortNames("result")]
+    [OutPortDescriptions("Result from the first derivative")]
+    [OutPortTypes("double")]
+    [IsDesignScriptCompatible]
+    public class FirstDerivative : NodeModel
+    {
+        internal EngineController EngineController { get; set; }
+
+        public FirstDerivative()
+        {
+            RegisterAllPorts();
+
+            ArgumentLacing = LacingStrategy.Disabled;
+            //MathNet.Numerics.Differentiate.FirstDerivative(f, x);
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            if (!HasConnectedInput(0) || !HasConnectedInput(1))
+            {
+                return new[]
+                {
+                    AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode())
+                };
+            }
+
+            var functioncall = AstFactory.BuildFunctionCall(
+                new Func<Func<double, double>, double, double>(MathNet.Numerics.Differentiate.FirstDerivative),
+                new List<AssociativeNode>
+                {
+                    inputAstNodes[0],
+                    inputAstNodes[1]
+                });
+
+            return new[]
+            {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), functioncall)
+            };
+        }
+    }*/
+}
