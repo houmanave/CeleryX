ef2655e76b551a14d5b362bb567d2097a0641a44
h-anave <hmpsnt00@pub.taisei.co.jp>
2019-06-14 22:34:29 +0900

    Version Up to 2019

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("y.rch")]
 [assembly: AssemblyProduct("Celery")]
-[assembly: AssemblyCopyright("Copyright © Homer Perseus Anave 2018")]
+[assembly: AssemblyCopyright("Copyright © Homer Perseus Anave 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.0520.0")]
+[assembly: AssemblyVersion("19.0701.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
